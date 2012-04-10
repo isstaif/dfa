881c58a52cc8f87337d72f1dea226c3c6bab6e4a
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-04-10 23:28:12 +0300

    modelling NFA

--- web.rb
+++ web.rb
@@ -1,14 +1,35 @@
 load 'dfa.rb'
-require 'matrix'
+
+keyword = "web"
+
+@nfa = Hash.new
+
+#an nfa
+@nfa = {
+  0 => Hash.new([0]).merge('w' => [0,1]),
+  1 => Hash.new([-1]).merge('e' => [2]),
+  2 => Hash.new([-1]).merge('b' => [3]),
+  3 => Hash.new([-1])
+}
+
+#initializing dfa
+@dfa = Hash.new
+
+@nfa.each do |state, transitions|
+  #puts "#{state} #{transitions}, default #{transitions.default}"
+  @dfa = @dfa.merge({state => 0})
+end
+
+puts @dfa
 
 #we have the states S, W, E, B
-d = DFA.new('S', ['B'])
+d = DFA.new(0, [3])
 
 @table = Hash.new
-@table = { "S" => Hash.new('S').merge({'w' => 'W'}),
-           "W" => Hash.new('S').merge({'e' => 'E'}),
-           "E" => Hash.new('S').merge({'b' => 'B'}),
-           "B" => Hash.new('S').merge({'b' => 'B'})
+@table = { 0 => Hash.new(0).merge({'w' => 1}),
+           1 => Hash.new(0).merge({'e' => 2}),
+           2 => Hash.new(0).merge({'b' => 3}),
+           3 => Hash.new(0).merge({'b' => 3})
          }
 
 d.transition do |s,a|
